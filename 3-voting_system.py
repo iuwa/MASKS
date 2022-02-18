6566abc4d26ce3d28d2685c8069d66b539c68090
Amirhoshang Hoseinpour Dehkordi <amir.hoseinpour@gmail.com>
2022-02-17 22:44:36 -0800

    Update 3-voting_system.py

--- 3-voting_system.py
+++ 3-voting_system.py
@@ -1,38 +1,29 @@
 import numpy as np
 import os
-from tensorflow.keras.datasets import cifar10, fashion_mnist
-import timeit
-import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-(input_train, target_train), (input_test, target_test) = fashion_mnist.load_data()
-class_names = ['Top cover', 'Trouser', 'Shoes', 'Bag']
-class_no = len(class_names)
-target_train = np.where(target_train==2, 0, target_train) 
-target_train = np.where(target_train==3, 0, target_train) 
-target_train = np.where(target_train==4, 0, target_train) 
-target_train = np.where(target_train==6, 0, target_train) 
-target_train = np.where(target_train==5, 2, target_train) 
-target_train = np.where(target_train==7, 2, target_train) 
-target_train = np.where(target_train==9, 2, target_train) 
-target_train = np.where(target_train==8, 3, target_train) 
-target_test = np.where(target_test==2, 0, target_test) 
-target_test = np.where(target_test==3, 0, target_test) 
-target_test = np.where(target_test==4, 0, target_test) 
-target_test = np.where(target_test==6, 0, target_test) 
-target_test = np.where(target_test==5, 2, target_test) 
-target_test = np.where(target_test==7, 2, target_test) 
-target_test = np.where(target_test==9, 2, target_test) 
-target_test = np.where(target_test==8, 3, target_test) 
-print(target_test[:100])
+label_dir = "../Fruit-Images-Dataset/MASKS/"
+data_dir = "../Fruit-Images-Dataset/Test/"
+path  = "../Fruit-Images-Dataset/MASKS/"
+Project_path  = "../Fruit-Images-Dataset/MASKS/"
 
-path  = "Results"
+datagen = ImageDataGenerator()
+test_it = datagen.flow_from_directory(
+                            data_dir, 
+                            seed=123)
+
+def load_labels():
+    label_arr = []
+    for _class_name in os.listdir(data_dir):
+        for _ in os.listdir(data_dir+_class_name):
+            label_arr.append(test_it.class_indices[_class_name])
+    return label_arr
+target_test = load_labels()
+class_no = len(test_it.class_indices)
 agent_counter = 0
-
 predictions = []
 for j in range(len(target_test)):
-    predictions.append([i for i in range(10)])
+    predictions.append([i for i in range(class_no)])
 agent_counter = 0
 results = []
 votes = []
@@ -40,9 +31,12 @@ for i in range(len(target_test)):
     votes.append({})
     for j in range(class_no):
         votes[i][j] = 0
-start_interval = timeit.default_timer()
-for dirr in os.listdir(path):
-    np_array = np.load(path+"/"+dirr)
+for i in range(len(target_test)):
+    votes.append({})
+    for j in range(class_no):
+        votes[i][j] = 0
+for dirr in os.listdir(path+"Results"):
+    np_array = np.load(path+"Results"+"/"+dirr)
     agent_counter +=1 
     correct_answer = 0
     wrong_answer = 0
@@ -50,17 +44,15 @@ for dirr in os.listdir(path):
     correct_assist = 0
     wrong_assist = 0
     for i in range(len(target_test)):
-        votes[i][np_array[i][0]] += 1
+        votes[i][np_array[i*51]] += 1
         max_vote_no = 0
         max_vote_index = []
-
         for j in range(class_no):
             if votes[i][j] > max_vote_no:
                 max_vote_no = votes[i][j]
                 max_vote_index = [j]
             elif votes[i][j] == max_vote_no:
                 max_vote_index.append(j)
-
         if not len(max_vote_index):
             conflict_answer += 1
         elif len(max_vote_index) == 1:
@@ -68,21 +60,11 @@ for dirr in os.listdir(path):
                 correct_answer += 1
             else:
                 wrong_answer += 1
-                # plt.figure(figsize=(10,10))
-                # plt.xticks([])
-                # plt.yticks([])
-                # plt.grid(False)
-                # plt.imshow(input_test[i])
-                # plt.xlabel("agent_counter-"+str(agent_counter)+"-instance-"+str(i)+"-"+class_names[predictions[i][0]]+"-but it is-"+class_names[target_test[i]])
-                # plt.show()
         else:
             if target_test[i] in max_vote_index:
                 correct_assist += 1
             else:
                 wrong_assist += 1
-    # with open("Agents/"+str(agent_counter),"w") as f:
-    #     f.write(str(predictions))
-    
     results.append({
         "agent_counter" : agent_counter,
         "correct_answer" : correct_answer,
@@ -90,9 +72,58 @@ for dirr in os.listdir(path):
         "conflict_answer" : conflict_answer,
         "correct_assist" : correct_assist,
         "wrong_assist" : wrong_assist})
-stop_interval = timeit.default_timer() 
-print("all agent Time Passed --------------------------------------------------------------------")
-print(stop_interval-start_interval)
-print(str(results).replace('},','},\n'))
-with open("AgentPredictions.txt","w") as f:
+with open(Project_path+"VotingAgentPredictions.txt","w") as f:
     f.write(str(results))
+vote_tresh = 0.99
+for vote_tresh_10 in range(1, 5):
+    vote_tresh = vote_tresh_10/100.0
+    agent_counter = 0
+    predictions = []
+    for j in range(len(target_test)):
+        predictions.append([i for i in range(class_no)])
+    agent_counter = 0
+    results = []
+    votes = []
+    for i in range(len(target_test)):
+        votes.append({})
+        for j in range(class_no):
+            votes[i][j] = 0
+    for dirr in os.listdir(path+"Results"):
+        np_array = np.load(path+"Results"+"/"+dirr)
+        agent_counter +=1 
+        correct_answer = 0
+        wrong_answer = 0
+        conflict_answer = 0
+        correct_assist = 0
+        wrong_assist = 0
+        for i in range(len(target_test)):
+            votes[i][np_array[i*51]] += 1
+            max_vote_no = 0
+            max_vote_index = []
+            for j in range(class_no):
+                if votes[i][j] > max_vote_no:
+                    max_vote_no = votes[i][j]
+                    max_vote_index = [j]
+                elif votes[i][j] == max_vote_no:
+                    max_vote_index.append(j)
+            if not len(max_vote_index):
+                conflict_answer += 1
+            elif len(max_vote_index) == 1 and votes[i][max_vote_index[0]]>=agent_counter*vote_tresh:
+                if max_vote_index[0] == target_test[i]:
+                    correct_answer += 1
+                else:
+                    wrong_answer += 1
+            else:
+                if target_test[i] in max_vote_index:
+                    correct_assist += 1
+                else:
+                    wrong_assist += 1
+        results.append({
+            "agent_counter" : agent_counter,
+            "correct_answer" : correct_answer,
+            "wrong_answer" : wrong_answer,
+            "conflict_answer" : conflict_answer,
+            "correct_assist" : correct_assist,
+            "wrong_assist" : wrong_assist})
+    with open(Project_path+str(vote_tresh)+"VotingAgentPredictions.txt","w") as f:
+        f.write(str(results))
